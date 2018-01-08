8988e5e26c6e14e68f896dc84c48d5851bec14cb
t-larson <tylerlarson1025@hotmail.com>
2018-01-07 20:23:21 -0800

    Rebalance TestSquare and change AI.

--- examplemod/monsters/TestSquare.java
+++ examplemod/monsters/TestSquare.java
@@ -2,6 +2,7 @@ package examplemod.monsters;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.AnimateSlowAttackAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.common.RollMoveAction;
@@ -16,11 +17,11 @@ public class TestSquare extends AbstractMonster {
     public static final String ID = "TestSquare";
     public static final String NAME = "Test Square";
     
-    public static final int HP_MAX = 24;
-    public static final int HP_MIN = 23;
+    public static final int HP_MAX = 36;
+    public static final int HP_MIN = 32;
     
-    public static final int HIT_DMG = 9;
-    public static final int GUARD_DMG = 4;
+    public static final int HIT_DMG = 11;
+    public static final int GUARD_DMG = 6;
     public static final int GUARD_BLK = 10;
     
     public static final float HBX = 0.0f;
@@ -32,6 +33,8 @@ public class TestSquare extends AbstractMonster {
     private static final byte HIT = 1;
     private static final byte GUARD = 2;
     
+    private boolean firstMove = true;
+    
     public TestSquare(float x, float y) {
         super(NAME, ID, AbstractDungeon.monsterHpRng.random(HP_MIN, HP_MAX), HBX, HBY, HBW, HBH, "status/beta", x, y);
         damage.add(new DamageInfo(this, HIT_DMG));
@@ -42,6 +45,8 @@ public class TestSquare extends AbstractMonster {
     
     @Override
     public void takeTurn() {
+        AbstractDungeon.actionManager.addToBottom(new AnimateSlowAttackAction(this));
+        
         switch (this.nextMove) {
             case HIT: {
                 AbstractDungeon.actionManager.addToBottom(new DamageAction((AbstractCreature)AbstractDungeon.player, (DamageInfo)this.damage.get(0), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
@@ -59,12 +64,16 @@ public class TestSquare extends AbstractMonster {
     
     @Override
     protected void getMove(int num) {
-        // 50/50 guard or hit
-        if (num >= 50) { 
-            setMove(GUARD, AbstractMonster.Intent.ATTACK_DEFEND, GUARD_DMG);
-        }
-        else {
+        if (firstMove) {
+            firstMove = false;
             setMove(HIT, AbstractMonster.Intent.ATTACK, HIT_DMG);
+        } else {
+            if (num >= 50) { 
+                setMove(GUARD, AbstractMonster.Intent.ATTACK_DEFEND, GUARD_DMG);
+            }
+            else {
+                setMove(HIT, AbstractMonster.Intent.ATTACK, HIT_DMG);
+            }
         }
     }
     
